a9c1683feb35f39c666b81e8b1338b4476ac38bb
Suraj N. Kurapati <sunaku@gmail.com>
2008-12-30 00:38:08 -0800

    initial public release of babelfish library

---
+++ Rakefile
@@ -0,0 +1,92 @@
+require 'rake/clean'
+
+##
+# documentation
+#
+
+desc "Build the documentation."
+task :doc
+
+# user manual
+task :doc => 'index.html'
+
+file 'index.html' => 'README' do |t|
+  sh 'maruku', t.prerequisites[0], '-o', t.name
+end
+
+CLOBBER.include 'index.html'
+
+# API reference
+task :doc => 'api'
+
+require 'rake/rdoctask'
+Rake::RDocTask.new 'api' do |t|
+  t.rdoc_dir = t.name
+  t.rdoc_files.exclude('pkg').include('**/*.rb')
+end
+
+
+##
+# packaging
+#
+
+require 'rake/gempackagetask'
+require 'lib/babelfish' # project info
+
+spec = Gem::Specification.new do |s|
+  s.rubyforge_project = 'sunaku'
+  s.author, s.email   = File.read('LICENSE').
+                        scan(/Copyright \d+ (.*) <(.*?)>/).first
+
+  s.name              = BabelFish::PROJECT
+  s.version           = BabelFish::VERSION
+  s.summary           = BabelFish::SUMMARY
+  s.description       = s.summary
+  s.homepage          = BabelFish::WEBSITE
+  s.files             = FileList['**/*']
+  s.executables       = s.name
+  s.has_rdoc          = true
+
+  # gems needed by the command line interface
+  s.add_dependency 'trollop', '~> 1.10'
+
+  # gems needed by the ruby library
+  s.add_dependency 'hpricot', '~> 0.6'
+end
+
+Rake::GemPackageTask.new(spec) do |pkg|
+  pkg.need_tar = true
+  pkg.need_zip = true
+end
+
+desc 'Build release packages.'
+task :pack => [:clobber, :doc] do
+  sh $0, 'package'
+end
+
+
+##
+# releasing
+#
+
+desc 'Upload to project website.'
+task :website => :doc do
+  sh "rsync -av index.html api ~/www/lib/#{spec.name} --delete"
+end
+
+desc 'Publish release packages.'
+task :publish => :pack do
+  sh 'rubyforge', 'login'
+
+  pusher = lambda do |cmd, pkg|
+    sh 'rubyforge', cmd, '--release_date', BabelFish::RELEASE,
+        spec.rubyforge_project, spec.name, spec.version.to_s, pkg
+  end
+
+  packs = Dir['pkg/*.[a-z]*']
+
+  # push ONLY the first package using 'add_release'.  otherwise, we
+  # get one package per sub-section on the RubyForge download page!
+  pusher['add_release', packs.shift]
+  packs.each {|pkg| pusher['add_file', pkg] }
+end
---
+++ lib/babelfish.rb
@@ -0,0 +1,75 @@
+require 'open-uri'
+require 'net/http'
+require 'uri'
+require 'set'
+
+require 'rubygems'
+require 'hpricot'
+
+# Ruby interface to Yahoo! BabelFish translation service.
+module BabelFish
+  ##
+  # project information
+  #
+
+  PROJECT = 'babelfish'
+  VERSION = '0.0.1'
+  RELEASE = '2008-12-30'
+  WEBSITE = "http://snk.tuxfamily.org/lib/#{PROJECT}"
+  SUMMARY = 'Ruby interface to Yahoo! BabelFish translation service.'
+  DISPLAY = PROJECT + ' ' + VERSION
+  INSTALL = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  ##
+  # service connection
+  #
+
+  # the URI through which the translation service is accessed
+  SERVICE_URI = URI.parse('http://babelfish.yahoo.com/translate_txt')
+
+  # Provides access to a language's name when given a language code.
+  LANGUAGE_NAMES = {} # code => name
+
+  # Provides access to a list of destination language codes when
+  # given a source language code.  In other words, this hash tells
+  # you the possible translations that are supported by the service.
+  LANGUAGE_PAIRS = Hash.new {|h,k| h[k] = Set.new } # code => [code]
+
+  # determine what translations are supported by the service
+  open(SERVICE_URI).read.
+  scan(/<option\s+value="(\w+)_(\w+)">(.*?)\s+to\s+(.*?)</).
+  each do |src_code, dst_code, src_name, dst_name|
+    LANGUAGE_NAMES[src_code] = src_name
+    LANGUAGE_NAMES[dst_code] = dst_name
+
+    LANGUAGE_PAIRS[src_code] << dst_code
+  end
+
+  # A list of possible language codes supported by the service.
+  LANGUAGE_CODES = LANGUAGE_PAIRS.keys
+
+  ##
+  # service interface
+  #
+
+  # Translates the given input from the given source
+  # language into the given destination language.
+  #
+  # input_text::        the text you want to translate
+  # input_lang_code::   the code of the language in
+  #                     which the input text is written
+  # output_lang_code::  language code of the result of translation
+  # output_encoding::   desired encoding of the result of translation
+  #
+  def BabelFish.translate input_text, input_lang_code, output_lang_code, output_encoding = 'utf-8'
+    output_page = Net::HTTP.post_form(
+      SERVICE_URI,
+      :eo => output_encoding,
+      :lp => "#{input_lang_code}_#{output_lang_code}",
+      :trtext => input_text.to_s
+    )
+
+    doc = Hpricot(output_page.body)
+    (doc / '#result' / 'div').inner_html
+  end
+end
